04de5d2e9ce7d024b689c9b270dfcdec20c06a7a
Julian Jark <juliuxu@gmail.com>
2020-12-02 22:57:22 +0100

    add sum util

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,7 @@
 import { test } from "./test"
 import { readInput } from "./readInput"
 
+export const sum = (numbers: number[]) => numbers.reduce((acc, x) => acc + x, 0)
+
 export { test, readInput }
 export default { test, readInput }
