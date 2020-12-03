d2467cc6bf09b2cf180d852a0beb6f3a26340375
Julian Jark <juliuxu@gmail.com>
2020-12-03 08:28:41 +0100

    day3

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,6 +2,7 @@ import { test } from "./test"
 import { readInput } from "./readInput"
 
 export const sum = (numbers: number[]) => numbers.reduce((acc, x) => acc + x, 0)
+export const multiply = (numbers: number[]) => numbers.reduce((acc, x) => acc * x, 1)
 
 export { test, readInput }
 export default { test, readInput }
