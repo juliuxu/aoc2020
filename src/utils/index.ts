860186ddabb75462e68b212dc91045efe5b3609a
Julian Jark <juliuxu@gmail.com>
2020-12-10 08:54:13 +0100

    make memo generic

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -5,11 +5,12 @@ export const sum = (numbers: number[]) => numbers.reduce((acc, x) => acc + x, 0)
 export const multiply = (numbers: number[]) => numbers.reduce((acc, x) => acc * x, 1)
 export const max = (numbers: number[]) => numbers.reduce((a, b) => Math.max(a, b))
 
-export function memo<A extends number, B>(f: (args: A) => B): (args: A) => B {
-  const d: Record<number, B> = {};
-  return (args: A) => {
-    if (!(args in d)) d[args] = f(args);
-    return d[args];
+export function memo<A, B>(f: (...args: A[]) => B): (args: A) => B {
+  const d: Record<string, B> = {};
+  return (...args: A[]) => {
+    const key = JSON.stringify(args)
+    if (!(key in d)) d[key] = f(...args);
+    return d[key];
   }
 }
 
